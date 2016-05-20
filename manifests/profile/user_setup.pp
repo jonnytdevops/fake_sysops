6631d1ac5093abcddb57fbcd9d7b88c3c0669522
Jonathan Tripathy <jt@puppetlabs.com>
2016-05-21 00:18:26 +0100

    Added ssh key access for user bob

--- manifests/profile/user_setup.pp
+++ manifests/profile/user_setup.pp
@@ -15,4 +15,10 @@ class fake_sysops::profile::user_setup {
     content => '%bob ALL=(ALL) ALL',
     require => Class['sudo'],
   }
+
+  ssh_authorized_key {'jonnyt@test.local':
+    user => 'bob',
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDim7Tga/JyYky8/E+EwOqf8Uzk2n/5nJN3E9ymKTgcBFlEmZo4aMew/e0JSZrTpCTj/gAFy2Nl+lLTAw19BSMCqPORtQ9dUqoPe2FJp882Jw/QRfqGdu+Ab37j/6kCQizvRDnqXYgrFLkMRvZkMJggB6PLIr4dePr8euCVhfee7MlT+tReZo/HvOFCPdlZWtQUISSKIGpqg7ywAEy2/ZheQ2e7KGjrgeg9RIlDBExWi/GbdcOWb9xOnta8QL6MSVxs5ptYLV3SWEGoy7gIeV0qpo2KEi91MA3m9+3CsbXtAmMVn3Z3FUpo8EBu1v/Woef9sDRSYimikGy+2VXpc+0h',
+  }
 }
