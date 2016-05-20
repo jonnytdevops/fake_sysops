cd7ede20d5ec7778f12f811eb91336e903f6d751
Jonathan Tripathy <jt@puppetlabs.com>
2016-05-20 21:59:30 +0100

    f

--- manifests/profile/dns.pp
+++ manifests/profile/dns.pp
@@ -8,48 +8,12 @@ class fake_sysops::profile::dns {
 
 	bind::zone {'fakesysops.me':
 		zone_type => 'master',
-		dynamic 	=> true,
+		source		=> 'puppet://modules/fake_sysops/dns/fakesysops.me-zone'
 	}
 
 	bind::view {'internal':
 		recursion	=> true,
 		match_destinations => ['127.0.0.1',],
 		zones 						 => ['fakesysops.me',],
-	}
-
-	resource_record { 'first.fakesysops.me':
-		ensure	=> present,
-		record  => 'first',
-		type 		=> 'A',
-		data		=> '1.1.1.1',
-		ttl			=> 86400,
-		zone 		=> 'fakesysops.me',
-	}
-
-	resource_record { 'second.fakesysops.me':
-		ensure	=> present,
-		record  => 'second',
-		type 		=> 'A',
-		data		=> '2.2.2.2',
-		ttl			=> 86400,
-		zone 		=> 'fakesysops.me',
-	}
-
-	resource_record { 'foo.fakesysops.me':
-		ensure	=> present,
-		record  => 'foo',
-		type 		=> 'A',
-		data		=> 'first',
-		ttl			=> 86400,
-		zone 		=> 'fakesysops.me',
-	}
-
-	resource_record { 'bar.fakesysops.me':
-		ensure	=> present,
-		record  => 'bar',
-		type 		=> 'A',
-		data		=> 'second',
-		ttl			=> 86400,
-		zone 		=> 'fakesysops.me',
 	}
 }
